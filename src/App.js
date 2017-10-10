64a1b428991fffd16e2ed8ce06ce7e30ec1ddfb1
prakashn27 <nprakashece@gmail.com>
2017-10-09 23:32:24 -0400

    added minutes and changed layout

--- src/App.js
+++ src/App.js
@@ -8,16 +8,22 @@ const StoryTile = (props) => {
         <div className="Title">{props.title}</div>
         <div className="Url">{props.url}</div>
       </a>
-      <div className="By">{props.by}</div>
+      <div>
+        <span>{props.points} points by <span className="By">{props.by}</span>  {props.time} mins ago</span>
+      </div>
     </div>
   )
 }
 class Story extends Component {
   initStory = (info) => {
+    let curTime = Date.now()/1000;
+    let delayMin = Math.round((curTime - info.time) / (60));
     this.setState({
       url : info.url,
       title : info.title,
-      by : info.by
+      by : info.by,
+      points : info.score,
+      time : delayMin
     });
   };
   componentDidMount() {
@@ -28,11 +34,6 @@ class Story extends Component {
   }
   constructor(props) {
     super(props);
-    this.state = {
-      title : "",
-      url : "",
-      by : ""
-    }
   }
   render() {
     return(
@@ -50,9 +51,23 @@ class StoryList extends Component {
       stories : []
     }
   }
-  rrender() {
+  initStories = (storylist) => {
+    console.log(storylist)
+    this.setState({
+      stories : storylist
+    });
+  };
+  componentDidMount() {
+    fetch(`https://hacker-news.firebaseio.com/v0/${this.props.title}.json`)
+    .then(res => res.json())
+    .then(text => this.initStories(text))
+    .catch((err) => console.log(err));
+  }
+  render() {
     return(
-        <div> stories </div>
+        <div>
+          {this.state.stories.map(story => <Story storyid={story}/>)}
+        </div>
     )
   }
 };
@@ -73,17 +88,28 @@ class Button extends Component {
   }
 };
 
+class TitleBar implements Component {
+
+  render() {
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
+
 class App extends Component {
-  state = { counter: 0 }
-  incrementCounter = (incrementValue) => {
-    this.setState((prevState) => ({
-      counter: prevState.counter + incrementValue
-    }));
+  state = { title: "beststories" }
+  setTitle = (newTitle) => {
+    this.setState({
+      title : newTitle
+    });
   };
   render() {
     return (
       <div>
-          <Story storyid={15435822} />
+          <StoryList title={this.state.title}/>
       </div>
     );
   }
